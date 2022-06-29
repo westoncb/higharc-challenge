78fe1b7720495b392ffe44e389ec2100f6e7af6a
Weston C. Beecroft <westoncb@gmail.com>
2022-06-29 02:39:45 -0700

    Highlight selected point
    and formatting

--- GraphGenerator.js
+++ GraphGenerator.js
@@ -3,7 +3,8 @@ const SNAP_RADIUS = 20
 class GraphGenerator {
     constructor() {
         const canvas = document.getElementById("generator-canvas")
-        const outputElement = document.getElementsByClassName("generator-output")[0]
+        const outputElement =
+            document.getElementsByClassName("generator-output")[0]
         let clickCount = 0
         let mousePos = []
         this.lineStarted = false
@@ -11,10 +12,9 @@ class GraphGenerator {
         this.edges = []
         this.canvasWidth = canvas.width
         this.canvasHeight = canvas.height
-        const ctx = canvas.getContext('2d')
+        const ctx = canvas.getContext("2d")
 
-        canvas.onclick = (e) => {
-
+        canvas.onclick = e => {
             if (!this.lineStarted) {
                 this.startLine(e.offsetX, e.offsetY)
             } else {
@@ -33,15 +33,15 @@ class GraphGenerator {
             this.printGraph(outputElement)
         }
 
-        canvas.onmousemove = (e) => {
+        canvas.onmousemove = e => {
             mousePos[0] = e.offsetX
             mousePos[1] = e.offsetY
 
             this.draw(ctx, mousePos)
         }
 
-        document.onkeyup = (e) => {
-            if (e.key === 'Escape') {
+        document.onkeyup = e => {
+            if (e.key === "Escape") {
                 this.undo()
                 clickCount = 0
                 this.lineStarted = false
@@ -60,7 +60,7 @@ class GraphGenerator {
     finishLine(x, y) {
         const newVert = this.snap([x, y])
         const vertIndex = this.addVertIfNecessary(newVert)
-        this.edges[this.edges.length-1].push(vertIndex)
+        this.edges[this.edges.length - 1].push(vertIndex)
     }
 
     /**
@@ -68,7 +68,9 @@ class GraphGenerator {
      * returns its new index. Otherwise returns old index.
      */
     addVertIfNecessary(newVert) {
-        let vertIndex = this.vertices.findIndex(vert => vert[0] === newVert[0] && vert[1] === newVert[1])
+        let vertIndex = this.vertices.findIndex(
+            vert => vert[0] === newVert[0] && vert[1] === newVert[1]
+        )
         if (vertIndex === -1) {
             this.vertices.push(newVert)
             vertIndex = this.vertices.length - 1
@@ -84,7 +86,13 @@ class GraphGenerator {
         // For each of those verts, only remove from vertices array
         // if this edge is the only one referencing it.
         lastEdge.forEach(vertIndex => {
-            const referenceCount = this.edges.reduce((total, edge) => (edge[0] === vertIndex || edge[1] === vertIndex) ? total + 1 : total, 0)
+            const referenceCount = this.edges.reduce(
+                (total, edge) =>
+                    edge[0] === vertIndex || edge[1] === vertIndex
+                        ? total + 1
+                        : total,
+                0
+            )
             if (referenceCount === 1) {
                 this.vertices.splice(vertIndex, 1)
             }
@@ -92,7 +100,11 @@ class GraphGenerator {
     }
 
     snap(vert) {
-        const target = this.vertices.find(curVert => (curVert[0] - vert[0])**2 + (curVert[1] - vert[1])**2 <= SNAP_RADIUS**2)
+        const target = this.vertices.find(
+            curVert =>
+                (curVert[0] - vert[0]) ** 2 + (curVert[1] - vert[1]) ** 2 <=
+                SNAP_RADIUS ** 2
+        )
 
         return target ? target : vert
     }
@@ -103,16 +115,27 @@ class GraphGenerator {
         ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
 
         this.vertices.forEach(vert => {
+            const dist = Math.sqrt(
+                (vert[0] - mousePos[0]) ** 2 + (vert[1] - mousePos[1]) ** 2
+            )
+            const withinSnapRadius = dist - SNAP_RADIUS <= 0
+            0
+
+            console.log("withinSnapRadius", dist, withinSnapRadius)
+
             ctx.beginPath()
             ctx.arc(vert[0], vert[1], vert_radius, 0, 2 * Math.PI, false)
-            ctx.fillStyle = 'black'
+            ctx.fillStyle = withinSnapRadius ? "#00ff66" : "black"
             ctx.fill()
         })
 
         ctx.strokeStyle = "black"
         this.edges.forEach((edge, i) => {
             const firstVert = this.vertices[edge[0]]
-            const secondVert = i === this.edges.length-1 && this.lineStarted ? mousePos : this.vertices[edge[1]]
+            const secondVert =
+                i === this.edges.length - 1 && this.lineStarted
+                    ? mousePos
+                    : this.vertices[edge[1]]
 
             ctx.beginPath()
             ctx.moveTo(firstVert[0], firstVert[1])
@@ -121,14 +144,14 @@ class GraphGenerator {
         })
 
         // show snap radius
-        ctx.beginPath();
-        ctx.strokeStyle = "#66aa66"
-        ctx.arc(mousePos[0], mousePos[1], SNAP_RADIUS, 0, 2 * Math.PI);
-        ctx.stroke();
+        ctx.beginPath()
+        ctx.strokeStyle = "#00aa66"
+        ctx.arc(mousePos[0], mousePos[1], SNAP_RADIUS, 0, 2 * Math.PI)
+        ctx.stroke()
     }
 
     printGraph(element) {
-        const data = {vertices: this.vertices, edges: this.edges}
+        const data = { vertices: this.vertices, edges: this.edges }
         element.innerHTML = `<p>${JSON.stringify(data, null, 2)}</p>`
     }
 }
